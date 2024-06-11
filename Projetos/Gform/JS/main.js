61c620bd3a27dd1ed1a927198219e1a241d27c70
Renato A. Silva <araujorenato045@gmail.com>
2024-06-11 10:27:03 -0400

    push

---
+++ Projetos/Gform/JS/main.js
@@ -0,0 +1,10 @@
+
+document.getElementById("texto").innerHTML="Meu primeiro texto utilizando <strong>JS<strong<></br>";
+
+console.log("Seu computador foi invadido");
+
+
+var a=1;
+var b=3
+var c=5;
+var d=(a+b)*c;
