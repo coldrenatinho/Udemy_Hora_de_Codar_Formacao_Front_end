05e1640318bd8708dfaeafaecbdea1f3f3e0e2c4
Renato A. Silva <97402553+coldrenatinho@users.noreply.github.com>
2024-11-27 21:59:18 -0400

    push

--- 2_Introducao_Java_Scrip/Projeto/Tabuada/js/script.js
+++ 2_Introducao_Java_Scrip/Projeto/Tabuada/js/script.js
@@ -2,14 +2,25 @@
 const multiplicationForm = document.querySelector(`#multiplication-form`);
 const numerador = document.querySelector(`#numerador`);
 const denominador = document.querySelector(`#denominador`);
-
 const mutiplicationTabler = document.querySelector(`#mutiplication-operator`);
+const show_numerador = document.querySelector(`#show-numerador`);
+
 //funções
 const createTable = (numerador, denominador) => {
   mutiplicationTabler.innerHTML = ``;
   for (let i = 1; i <= numerador; i++) {
-    console.log(denominador * i);
+    const result = denominador * i;
+    const template = `<div class="row">
+        <div class="operation">${denominador} x ${i} = </div>
+        <div class="result">${result}</div>
+        </div>`;
+    const parser = new DOMParser(); //transforma uma string em um documento html
+    const htmlTemplate = parser.parseFromString(template, `text/html`);
+    const row = htmlTemplate.querySelector(`.row`);
+    mutiplicationTabler.appendChild(row);
   }
+
+  show_numerador.innerHTML = denominador;
 };
 
 //eventos
@@ -17,13 +28,10 @@ multiplicationForm.addEventListener(`submit`, (event) => {
   event.preventDefault();
   const numeradorValue = numerador.value;
   const denominadorValue = denominador.value;
-  if (!numeradorValue || !denominadorValue) {
+  if (!numeradorValue || !denominadorValue || denominadorValue > 100) {
     alert(`Por favor, preencha os campos corretamente!`);
     return;
   } else {
-    const result = numeradorValue * denominadorValue;
-    console.log(numeradorValue, denominadorValue, result);
+    createTable(numeradorValue, denominadorValue);
   }
-
-  createTable(numeradorValue, denominadorValue);
 });
