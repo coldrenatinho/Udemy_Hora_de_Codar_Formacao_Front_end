41649cceebbed8c1c92146643a5daf8b7f55dbaf
Renato A. Silva <97402553+coldrenatinho@users.noreply.github.com>
2024-12-02 11:58:13 -0400

    push'

--- 2_Introducao_Java_Scrip/Projeto/CopiaPaginaIphone/js/scripts.js
+++ 2_Introducao_Java_Scrip/Projeto/CopiaPaginaIphone/js/scripts.js
@@ -1,1 +1,16 @@
 console.log("O script está funcionando!");
+
+//Busca o elemento pelo ID
+const buttons = document.querySelectorAll("#image-picker li");
+
+//Busca o elemento pelo ID
+const image = document.querySelector("#product-image");
+
+//Adiciona um evento de click para cada botão
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    buttons.forEach((btn) => {
+      btn.querySelector(".color").classList.remove("selected");
+    });
+  });
+});
